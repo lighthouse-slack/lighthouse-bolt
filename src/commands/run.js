01ef8e2799b7609481540755611e8985ff58b651
Eivenlour David <eivenlou@ualberta.ca>
2020-10-30 04:39:08 -0600

    fix: indentations

--- src/commands/run.js
+++ src/commands/run.js
@@ -16,15 +16,16 @@ const getReportMessage = async (say, url) => {
 
 const runCommand = async (say, payload) => {
 
-  const url = payload.text.split(' ')[1];  
+  const url = payload.text.split(' ')[1];
+  
   if (!(validateURL(url))) {
     await say(`The URL you\'ve entered (${url}) is invalid. Please try again.`)
     return;
   }
 
-	await sendLoadingMessage(say, payload);
-	const reportMessage = await getReportMessage(say, url);
-	await say(reportMessage);
+  await sendLoadingMessage(say, payload);
+  const reportMessage = await getReportMessage(say, url);
+  await say(reportMessage);
 
   return;
 };
