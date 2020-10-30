94e111b34d6fd7265b615c533f73754a504c24cd
Helen Gezahegn <gezahegn@ualberta.ca>
2020-10-30 02:46:57 -0600

    chore: project restructure (#2)
    - moved lighthouse-bolt logic to help.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const { App } = require("@slack/bolt");
+const mainCommand = require("./src/commands/main");
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
@@ -10,64 +11,11 @@ const app = new App({
 });
 
 app.command("/lighthouse-bolt", async ({ ack, payload, context }) => {
-  // Acknowledge the command request
-  ack();
-
-  try {
-    const result = await app.client.views.open({
-      token: context.botToken,
-      trigger_id: payload.trigger_id, // Pass a valid trigger_id within 3 seconds of receiving it
-      view: {
-        type: "modal",
-        callback_id: "view_1",
-        title: {
-          type: "plain_text",
-          text: "Modal title",
-        },
-        blocks: [
-          {
-            type: "section",
-            text: {
-              type: "mrkdwn",
-              text: "Welcome to a modal with _blocks_",
-            },
-            accessory: {
-              type: "button",
-              text: {
-                type: "plain_text",
-                text: "Click me!",
-              },
-              action_id: "button_abc",
-            },
-          },
-          {
-            type: "input",
-            block_id: "test_input",
-            label: {
-              type: "plain_text",
-              text: "What are your hopes and dreams?",
-            },
-            element: {
-              type: "plain_text_input",
-              action_id: "dreamy_input",
-              multiline: true,
-            },
-          },
-        ],
-        submit: {
-          type: "plain_text",
-          text: "Submit",
-        },
-      },
-    });
-    console.log("\nPAYLOAD:\n", payload);
-  } catch (error) {
-    console.error(error);
-  }
+  ack(); // Acknowledge the command request
+  await mainCommand(app, payload, context);
 });
 
 (async () => {
   await app.start(process.env.PORT || 3000);
-
   console.log("⚡️ Bolt app is running!");
 })();
