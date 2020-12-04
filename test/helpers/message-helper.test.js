15566332b9a4e0546fbf73e144bad26a47a3014f
Eivenlour David <eivenlou@ualberta.ca>
2020-12-03 20:09:46 -0700

    fix: indentation

--- test/helpers/message-helper.test.js
+++ test/helpers/message-helper.test.js
@@ -2,86 +2,86 @@ const { sendMessage, deleteMessage } = require('../../src/helpers/message-helper
 
 describe('MessageHelper Tests', () => {
 
-	let credentials, app, postMessageSpy, deleteSpy, consoleErrorSpy;
+  let credentials, app, postMessageSpy, deleteSpy, consoleErrorSpy;
 
-	beforeEach(()=> {
-		postMessageSpy = jest.fn();
-		deleteSpy = jest.fn();
-		consoleErrorSpy = jest.fn();
-		app = {
-			client: {
-				chat: {
-					postMessage: postMessageSpy,
-					delete: deleteSpy
-				}
-			}
-		}
-		credentials = {
-			app,
-			token: Math.random(),
-			channel: Math.random()
-		}
-		window.console = {
-			error: consoleErrorSpy
-		}
-	});
+  beforeEach(()=> {
+    postMessageSpy = jest.fn();
+    deleteSpy = jest.fn();
+    consoleErrorSpy = jest.fn();
+    app = {
+      client: {
+        chat: {
+          postMessage: postMessageSpy,
+          delete: deleteSpy
+        }
+      }
+    }
+    credentials = {
+      app,
+      token: Math.random(),
+      channel: Math.random()
+    }
+    window.console = {
+      error: consoleErrorSpy
+    }
+  });
 
-	it('should call app.client.chat.postMessage with proper arguments when sendMessage is called', async () => {
-		const message = 'Test';
-		await sendMessage(credentials, { text: message });
-		await expect(app.client.chat.postMessage).toBeCalledWith({
-			token: credentials.token,
-			channel: credentials.channel,
-			text: message
-		});
-	});
+  it('should call app.client.chat.postMessage with proper arguments when sendMessage is called', async () => {
+    const message = 'Test';
+    await sendMessage(credentials, { text: message });
+    await expect(app.client.chat.postMessage).toBeCalledWith({
+      token: credentials.token,
+      channel: credentials.channel,
+      text: message
+    });
+  });
 
-	it('should call console.error when sendMessage is called and error is caught', async () => {
-		const err = new Error();
-		const throwErrorSpy = jest.fn().mockImplementation(() => { throw err });
-		const newApp = {
-			client: {
-				chat: {
-					postMessage: throwErrorSpy
-				}
-			}
-		}
-		const newCredentials = {
-			...credentials,
-			app: newApp
-		}
-		const message = 'Test';
-		await sendMessage(newCredentials, { text: message });
-		expect(console.error).toBeCalledWith(err);
-	});
+  it('should call console.error when sendMessage is called and error is caught', async () => {
+    const err = new Error();
+    const throwErrorSpy = jest.fn().mockImplementation(() => { throw err });
+    const newApp = {
+      client: {
+        chat: {
+          postMessage: throwErrorSpy
+        }
+      }
+    }
+    const newCredentials = {
+      ...credentials,
+      app: newApp
+    }
+    const message = 'Test';
+    await sendMessage(newCredentials, { text: message });
+    expect(console.error).toBeCalledWith(err);
+  });
 
-	it('should call app.client.chat.delete with proper arguments when deleteMessage is called', async () => {
-		const ts = '0123456789';
-		await deleteMessage(credentials, ts);
-		await expect(app.client.chat.delete).toBeCalledWith({
-			token: credentials.token,
-			channel: credentials.channel,
-			ts
-		});
-	});
+  it('should call app.client.chat.delete with proper arguments when deleteMessage is called', async () => {
+    const ts = '0123456789';
+    await deleteMessage(credentials, ts);
+    await expect(app.client.chat.delete).toBeCalledWith({
+      token: credentials.token,
+      channel: credentials.channel,
+      ts
+    });
+  });
 
-	it('should call console.error when deleteMessage is called and error is caught', async () => {
-		const err = new Error();
-		const throwErrorSpy = jest.fn().mockImplementation(() => { throw err });
-		const newApp = {
-			client: {
-				chat: {
-					delete: throwErrorSpy
-				}
-			}
-		}
-		const newCredentials = {
-			...credentials,
-			app: newApp
-		}
-		const ts = '0123456789';
-		await deleteMessage(newCredentials, ts);
-		expect(console.error).toBeCalledWith(err);
-	});
+  it('should call console.error when deleteMessage is called and error is caught', async () => {
+    const err = new Error();
+    const throwErrorSpy = jest.fn().mockImplementation(() => { throw err });
+    const newApp = {
+      client: {
+        chat: {
+          delete: throwErrorSpy
+        }
+      }
+    }
+    const newCredentials = {
+      ...credentials,
+      app: newApp
+    }
+    const ts = '0123456789';
+    await deleteMessage(newCredentials, ts);
+    expect(console.error).toBeCalledWith(err);
+  });
 
 });
--- test/helpers/url-helper.test.js
+++ test/helpers/url-helper.test.js
@@ -3,47 +3,47 @@ const TinyURL = require('tinyurl');
 
 describe('UrlHelper Tests', () => {
 
-	let credentials, app, postMessageSpy;
+  let credentials, app, postMessageSpy;
 
-	beforeEach(()=> {
-		postMessageSpy = jest.fn();
-		app = {
-			client: {
-				chat: {
-					postMessage: postMessageSpy
-				}
-			}
-		}
-		credentials = {
-			app,
-			token: Math.random(),
-			channel: Math.random()
-		}
-	});
+  beforeEach(()=> {
+    postMessageSpy = jest.fn();
+    app = {
+      client: {
+        chat: {
+          postMessage: postMessageSpy
+        }
+      }
+    }
+    credentials = {
+      app,
+      token: Math.random(),
+      channel: Math.random()
+    }
+  });
 
-	it('should return true if url is valid  when validateUrl is called', () => {
-		const testUrl = 'https://example.com';
-		expect(validateURL(testUrl)).toBeTruthy();
-	});
+  it('should return true if url is valid  when validateUrl is called', () => {
+    const testUrl = 'https://example.com';
+    expect(validateURL(testUrl)).toBeTruthy();
+  });
 
-	it('should return false if url is invalid when validateUrl is called', () => {
-		const testUrl = 'invalid url';
-		expect(validateURL(testUrl)).toBeFalsy();
-	});
+  it('should return false if url is invalid when validateUrl is called', () => {
+    const testUrl = 'invalid url';
+    expect(validateURL(testUrl)).toBeFalsy();
+  });
 
-	it('should return shortURL when shortenURL is called', async () => {
-		const testUrl = 'https://example.com';
-		const testTinyUrl = 'https://tinyurl.com/test';
-		TinyURL.shorten = jest.fn().mockImplementation(() => testTinyUrl);
-		expect(await shortenURL(credentials, testUrl)).toEqual(testTinyUrl);
-	});
+  it('should return shortURL when shortenURL is called', async () => {
+    const testUrl = 'https://example.com';
+    const testTinyUrl = 'https://tinyurl.com/test';
+    TinyURL.shorten = jest.fn().mockImplementation(() => testTinyUrl);
+    expect(await shortenURL(credentials, testUrl)).toEqual(testTinyUrl);
+  });
 
-	it('should return nothing when shortenURL is called and error is caught', async () => {
-		const testUrl = 'https://example.com';
-		const err = new Error();
-		const throwErrorSpy = jest.fn().mockImplementation(() => { throw err });
-		TinyURL.shorten = throwErrorSpy;
-		expect(await shortenURL(credentials, testUrl)).toEqual();
-	});
+  it('should return nothing when shortenURL is called and error is caught', async () => {
+    const testUrl = 'https://example.com';
+    const err = new Error();
+    const throwErrorSpy = jest.fn().mockImplementation(() => { throw err });
+    TinyURL.shorten = throwErrorSpy;
+    expect(await shortenURL(credentials, testUrl)).toEqual();
+  });
 
 });
